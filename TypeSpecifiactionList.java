f187e648cf88ef0a1c8866c3bdb77506fd67c49d
Manuel Rajesh <manuelrajesh39@gmail.com>
2026-03-25 09:36:41 +0530

    Add files via upload

---
+++ TypeSpecifiactionList.java
@@ -0,0 +1,26 @@
+package collextion;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TypeSpecifiactionList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> lst=new ArrayList<String>();
+		lst.add("hi");
+		lst.add("manuel");
+		lst.add("rajesh");
+		Iterator itr=lst.iterator();
+		while(itr.hasNext()) {
+			if(itr.next().equals("hi")) {
+				itr.remove();
+			}
+		}
+		System.out.println(lst);
+
+	}
+
+}
