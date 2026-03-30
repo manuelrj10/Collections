46bacdc31f21f3f9ac2f98cf669d9251bc051dda
Manuel Rajesh <manuelrajesh39@gmail.com>
2026-03-30 10:17:23 +0530

    Add files via upload

---
+++ Employee1.java
@@ -0,0 +1,13 @@
+package collextion;
+
+public class Employee1 {
+	int empNo;
+	String empName;
+	int empAge;
+	Employee1(int empNo,String empName,int empAge){
+		this.empNo=empNo;
+		this.empName=empName;
+		this.empAge=empAge;
+	}
+
+}
